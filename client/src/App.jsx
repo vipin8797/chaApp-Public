1bf7b749ef11718afbded940efbc5083ea3caf1a
vipin <vy879736@gmail.com>
2025-12-02 17:42:48 +0530

    feat(router): add full routing setup and integrate BrowserRouter with App and main entry

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
