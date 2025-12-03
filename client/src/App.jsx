f9fae3e492e2985b8352e464c553881ede911c91
vipin <vy879736@gmail.com>
2025-12-03 12:33:47 +0530

    style(app): apply universal background styling across all components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,13 @@ import ProfilePage from "./pages/ProfilePage";
 const App = () => {
   return (
     <>
+    <div className='dark:bg-gray-900'>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
+      </div>
     </>
   );
 };
