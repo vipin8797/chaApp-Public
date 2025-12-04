042cfaa5c66d626d33777b686784d7e2eb3930d4
vipin <vy879736@gmail.com>
2025-12-04 12:53:39 +0530

    refactor(socket): clean up and format Socket.io initialization code in app.js

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,52 @@ import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
-
+import {Server} from "socket.io";
 
 
 
 const app = express();
 const server = http.createServer(app);
+
+//Initialize socket.io server
+export const io = new Server(server, {
+  cors:{
+    origin: "*",
+  }
+})
+
+// Store Online Users:  { userId: socketId }
+export const userSocketMap = {};
+
+// Socket.io connection handler
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+
+  console.log("User Connected:", userId);
+
+  // Add user to map
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
+  // Send online users to all clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", userId);
+
+    // Remove from map
+    if (userId) {
+      delete userSocketMap[userId];
+    }
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+
+
 
 // Midddlewares
 app.use(express.json({ limit: "4mb" }));
