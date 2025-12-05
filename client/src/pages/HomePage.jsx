2b40faf17c6d1abc1d35572bec79d02bc95acdd6
vipin <vy879736@gmail.com>
2025-12-05 15:23:49 +0530

    refactor(home): remove hardcoded props and clean up HomePage structure

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -26,7 +26,7 @@ const HomePage = () => {
             : "md:grid-cols-2"
         }`}
       >
-        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <Sidebar  />
         <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
         <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
       </div>
