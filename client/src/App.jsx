920ab890f3a244b4d06fb3735d935f437e8fcfb7
Vipin <vy879736@gmail.com>
2026-05-25 19:35:24 +0530

    style(client): use double quotes for class names consistently
    - Update wrapper div className quotes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const {authUser} = useContext(AuthContext);
   return (
     <>
-    <div className='dark:bg-gray-900'>
+    <div className="dark:bg-gray-900">
       <Toaster />
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
